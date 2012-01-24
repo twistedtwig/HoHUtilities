7be614935c4303ef2b80ffba39880157b6b174fd
Jon Hawkins <jonathan1@houseofhawkins.com>
2012-01-24 19:43:13 +0000

    Added CSS and JavaScript extension html helper methods and tests.

--- Mvc.Html.Helpers.Test/HelperExtensions.cs
+++ Mvc.Html.Helpers.Test/HelperExtensions.cs
@@ -15,5 +15,25 @@ namespace Mvc.Html.Helpers.Test
 
             Assert.AreEqual("<img alt=\"some alt text\" src=\"this/is/my/source.jpg\" />", htmlString.ToHtmlString());
         }
+
+
+        [Test]
+        public void TestImageMvcHtmlHelperReturnsCorrectScriptTagText()
+        {
+            HtmlHelper helper = null;
+            MvcHtmlString htmlString = helper.JavaScript("this/is/my/source.js");
+
+            Assert.AreEqual("<script src=\"this/is/my/source.js\" type=\"text/javascript\" />", htmlString.ToHtmlString());
+        }
+
+
+        [Test]
+        public void TestImageMvcHtmlHelperReturnsCorrectCSSTagText()
+        {
+            HtmlHelper helper = null;
+            MvcHtmlString htmlString = helper.CSS("this/is/my/source.css");
+
+            Assert.AreEqual("<link href=\"this/is/my/source.css\" rel=\"stylesheet\" type=\"text/css\" />", htmlString.ToHtmlString());
+        }
     }
 }
--- Mvc.Html.Helpers/HelperExtensions.cs
+++ Mvc.Html.Helpers/HelperExtensions.cs
@@ -18,5 +18,36 @@ namespace Mvc.Html.Helpers
             builder.MergeAttribute("alt", altText);
             return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
         }
+
+
+        /// <summary>
+        /// Extension method on the HTML helper for razor that will allow for the creation of an html script tag without having to type it all manually
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static MvcHtmlString JavaScript(this HtmlHelper helper, string src)
+        {
+            var builder = new TagBuilder("script");
+            builder.MergeAttribute("type", "text/javascript");
+            builder.MergeAttribute("src", src);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+
+
+        /// <summary>
+        /// Extension method on the HTML helper for razor that will allow for the creation of an html CSS tag without having to type it all manually
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static MvcHtmlString CSS(this HtmlHelper helper, string src)
+        {
+            var builder = new TagBuilder("link");
+            builder.MergeAttribute("rel", "stylesheet");
+            builder.MergeAttribute("type", "text/css");
+            builder.MergeAttribute("href", src);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
     }
 }
