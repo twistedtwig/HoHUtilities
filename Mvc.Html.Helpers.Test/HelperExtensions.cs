39c95ef9b5cecd9ec64f7db13540ce16a5c585fc
Jon Hawkins <jonathan1@houseofhawkins.com>
2012-01-24 19:25:24 +0000

    Added MVC HTML helpers project and test projects to HoH utils
    Signed-off-by: Jon Hawkins <jonathan1@houseofhawkins.com>

---
+++ Mvc.Html.Helpers.Test/HelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace Mvc.Html.Helpers.Test
+{
+    [TestFixture]
+    public class HelperExtensions
+    {
+
+        [Test]
+        public void TestImageMvcHtmlHelperReturnsCorrectImgTagText()
+        {
+            HtmlHelper helper = null;
+            MvcHtmlString htmlString = helper.Image("this/is/my/source.jpg", "some alt text");
+
+            Assert.AreEqual("<img alt=\"some alt text\" src=\"this/is/my/source.jpg\" />", htmlString.ToHtmlString());
+        }
+    }
+}
---
+++ Mvc.Html.Helpers/HelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Mvc.Html.Helpers
+{
+    public static class HelperExtensions
+    {
+        /// <summary>
+        /// Extension method on the HTML helper for razor that will allow for the creation of an html img link without having to type it all manually
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="src"></param>
+        /// <param name="altText"></param>
+        /// <returns></returns>
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
