4bb4b46b3a9b09350f09d67511109d770a17f3fd
Jon Hawkins <jonathan1@houseofhawkins.com>
2012-01-26 20:50:24 +0000

    Added method to convert a partial view to text using razor stuff.

---
+++ Mvc.Html.Helpers/Razor.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace Mvc.Html.Helpers
+{
+    public class Razor
+    {
+        /// <summary>
+        /// Takes the given view and turns the output into a string to be used with Razor, partialviews and Json.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="viewData"></param>
+        /// <param name="tempData"></param>
+        /// <param name="viewName"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string RenderRazorViewToString(ControllerContext context, ViewDataDictionary viewData, TempDataDictionary tempData, string viewName, object model)
+        {
+            viewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(context, viewName);
+                var viewContext = new ViewContext(context, viewResult.View, viewData, tempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(context, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
