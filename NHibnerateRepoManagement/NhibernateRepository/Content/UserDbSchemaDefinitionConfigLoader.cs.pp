357b119f8ffd990cb538851e302ca1a01aa444b8
Jon Hawkins <jonathan1@houseofhawkins.com>
2013-12-28 14:57:10 +0000

    initial setup of NHibernateRepo project

---
+++ NHibnerateRepoManagement/NhibernateRepository/Content/UserDbSchemaDefinitionConfigLoader.cs.pp
@@ -0,0 +1,17 @@
+﻿
+namespace $rootnamespace$
+{
+    public class UserDbSchemaDefinitionConfigLoader : DbSchemaDefinitionConfigLoader
+    {
+
+        /// <summary>
+        /// The base class will try to load a config section called "DbSchemaDefinition", if none is found it will create a blank model.
+        /// You can override the loader with your own implementation here if required.
+        /// </summary>
+        /// <returns></returns>
+        public override DbSchemaDefinition CreateDefinition()
+        {
+            return base.CreateDefinition();
+        }
+    }
+}
---
+++ NHibnerateRepoManagement/NhibernateRepository/DbSchemaDefinitionConfigLoader.cs
@@ -0,0 +1,21 @@
+﻿using CustomConfigurations;
+using System;
+
+namespace NhibernateRepository
+{
+    public abstract class DbSchemaDefinitionConfigLoader : IDefinitionLoader
+    {
+        public virtual DbSchemaDefinition CreateDefinition()
+        {
+            try
+            {
+                return new Config().GetSection("DbSchemaDefinition").Create<DbSchemaDefinition>();
+            }
+            catch (Exception)
+            {
+                //incase no definition setup
+                return new DbSchemaDefinition();
+            }
+        }
+    }
+}
