29cc631f56bcbf40ab31d495d1d4bce85a6a9097
Jon Hawkins <jonathan1@houseofhawkins.com>
2013-12-28 19:36:36 +0000

    updated the base repo so that users do not have to deal with generic inheritance.

---
+++ NHibnerateRepoManagement/NhibernateRepository/Content/DbRepository.cs.pp
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Linq;
+using NhibernateRepository;
+
+namespace NhibernateRepository.Content
+{
+    public class DbRepository<T, U> : RepositoryBase<T, U> where T : IDefinitionLoader where U : ISchemaConfigurationLoader
+    {
+
+        public DbRepository(T definitionLoader, U schemaConfigurationLoader) : base(definitionLoader, schemaConfigurationLoader)
+        {
+
+        }
+        
+    }
+}
