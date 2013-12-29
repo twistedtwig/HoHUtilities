58eb11483f91089e853963f51752311b161b09c9
Jon Hawkins <jonathan1@houseofhawkins.com>
2013-12-29 17:10:04 +0000

    added add resources for razor.

--- Web-Utils/Mvc.Html.Helpers/Razor.cs
+++ Web-Utils/Mvc.Html.Helpers/Razor.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
 namespace Mvc.Html.Helpers
 {
-    public class Razor
+    public static class Razor
     {
         /// <summary>
         /// Takes the given view and turns the output into a string to be used with Razor, partialviews and Json.
@@ -25,6 +29,45 @@ namespace Mvc.Html.Helpers
                 viewResult.ViewEngine.ReleaseView(context, viewResult.View);
                 return sw.GetStringBuilder().ToString();
             }
+        }
+
+        public static IHtmlString AddResource(this HtmlHelper htmlHelper, Func<object, HelperResult> template, string type)
+        {
+            return AddResource(htmlHelper, template(null).ToString(), type);
+        }
+
+        public static IHtmlString AddResource(this HtmlHelper htmlHelper, string text, string type)
+        {
+            if (htmlHelper.ViewContext.HttpContext.Items[type] != null)
+            {
+                var list = (List<string>)htmlHelper.ViewContext.HttpContext.Items[type];
+                if (!list.Contains(text))
+                {
+                    list.Add(text);
+                }
+            }
+            else
+            {
+                htmlHelper.ViewContext.HttpContext.Items[type] = new List<string> { text };
+            }
+
+            return new HtmlString(String.Empty);
+        }
+
+
+        public static IHtmlString RenderResources(this HtmlHelper htmlHelper, string type)
+        {
+            if (htmlHelper.ViewContext.HttpContext.Items[type] != null)
+            {
+                var resources = (List<string>)htmlHelper.ViewContext.HttpContext.Items[type];
+
+                foreach (var resource in resources)
+                {
+                    if (resource != null) htmlHelper.ViewContext.Writer.Write(resource);
+                }
+            }
+
+            return new HtmlString(String.Empty);
         }
     }
 }
